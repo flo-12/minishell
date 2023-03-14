101b32ec91a0b92af30229c51cb5cdb479197bb7
Marco Vomiero <mvomiero@student.42berlin.de>
2023-03-14 16:44:09 +0100

    main update

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,17 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 14:47:51 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/03/14 14:49:36 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/03/14 16:19:02 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(void)
+int main(int ac, char **av, char **env)
 {
+	(void)av;
+	(void)env;
+	if (ac != 1)
+		return (ft_putendl_fd("minishell doesn't accept arguments!", 2), 1);
 	printf("hello world!\n");
 }
