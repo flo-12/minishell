863f6f7d5d4d25c9ae3d5f74eebe0e4c75fe265f
Florian Becht <fbecht@c4b8c1.42berlin.de>
2023-04-21 13:09:45 +0200

    adding folder and file with first prototypes of functions for signalhandling

---
+++ srcs/signals/signals.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbecht <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/21 13:07:20 by fbecht            #+#    #+#             */
+/*   Updated: 2023/04/21 13:07:22 by fbecht           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+//#include "../../includes/minishell_flo.h"
+
+/*
+*/
+void	ignore_sigquit()
+{
+
+}
+
+/*
+*/
+void	signal_interactive()
+{
+	//ignore_sigquit();
+}
+
+/*
+*/
+void	signal_non_interactive()
